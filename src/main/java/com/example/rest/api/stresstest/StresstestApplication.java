8863765b441e3985745a66926b0b17663b72f2f4
Victor Lee <victorlee0505@gmail.com>
2021-02-16 14:42:00 -0500

    Main: Enable Async

--- src/main/java/com/example/rest/api/stresstest/StresstestApplication.java
+++ src/main/java/com/example/rest/api/stresstest/StresstestApplication.java
@@ -1,13 +1,29 @@
 package com.example.rest.api.stresstest;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class StresstestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StresstestApplication.class, args);
 	}
+
+	@Bean(name="apiExecutor")
+    public Executor apiExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(16);
+        // executor.setMaxPoolSize(250);
+		// executor.setQueueCapacity(10);
+        executor.initialize();
+        return executor;
+    }
 
 }
